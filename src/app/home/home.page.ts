1a6379cd68c5e677a8fd1e596dfc1a05de20ad8d
Jack <50391717+pppp0836@users.noreply.github.com>
2020-06-24 16:23:11 +0800

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-// import Keyboard from "simple-keyboard";
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -9,14 +8,8 @@ export class HomePage {
   curScore: string = '';
   tinyKey: string[] = ['0','1','2','3','4','5','6','DEL'];
   selectedIndex: number;
-  private value: string;
-  // keyboard: Keyboard;
 
   constructor() {
-  }
-
-  handleZoomEvent($event: any) {
-    console.log($event)
   }
 
   onImgDidLoad() {
@@ -52,37 +45,4 @@ export class HomePage {
     this.curScore = '';//this.curScore.slice(0, -1);
   }
 
-  ngAfterViewInit() {
-    // this.keyboard = new Keyboard({
-    //
-    //   onChange: input => this.onChange(input),
-    //   onKeyPress: button => this.onKeyPress(button)
-    // });
-  }
-  // onChange = (input: string) => {
-    // this.value = input;
-    // console.log("Input changed", input);
-  // };
-
-  // onKeyPress = (button: string) => {
-  //   console.log("Button pressed", button);
-
-    /**
-     * If you want to handle the shift and caps lock buttons
-     */
-    // if (button === "{shift}" || button === "{lock}") this.handleShift();
-  // };
-
-  // onInputChange = (event: any) => {
-  //   this.keyboard.setInput(event.target.value);
-  // };
-  //
-  // handleShift = () => {
-  //   let currentLayout = this.keyboard.options.layoutName;
-  //   let shiftToggle = currentLayout === "default" ? "shift" : "default";
-  //
-  //   this.keyboard.setOptions({
-  //     layoutName: shiftToggle
-  //   });
-  // };
 }
